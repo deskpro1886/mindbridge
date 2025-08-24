853e55d41eaa6efdc868cf6d932b27b40a5d5464
deskpro1886 <95053464+deskpro1886@users.noreply.github.com>
2025-08-24 09:38:17 +0100

    Add files via upload

---
+++ Minbridge.sh
@@ -0,0 +1,6 @@
+# 1. Clone the empty repo
+git clone https://github.com/yourusername/mindbridge.git
+cd mindbridge
+
+# 2. Add all the files we designed
+# (You can copy the files from earlier or use the template below)
